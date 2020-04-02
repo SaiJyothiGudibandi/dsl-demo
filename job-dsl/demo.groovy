c7e34963393eb09a30fa03010617c81e1ffdefd3
SaiJyothiGudibandi <saijyothigudibandi9@gmail.com>
2020-04-02 11:35:39 -0700

    Update demo.groovy

--- job-dsl/demo.groovy
+++ job-dsl/demo.groovy
@@ -1,6 +1,9 @@
 pipelineJob('DSL_Demo') {
 
     def repo = 'https://github.com/SaiJyothiGudibandi/sample-project-maven.git'
+    parameters {
+        stringParam('var1', '$var1')
+    }
    
     triggers {
         scm('*/15 * * * *')
